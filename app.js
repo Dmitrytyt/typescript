2097d0b6c716a2880124d232d08cccc3b06b9848
Dmitry Turchenko <dmitrytyt@ya.ru>
2023-12-19 22:12:42 +0300

    Add 4-dummy-api

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 "use strict";
-const a = 1;
+Object.defineProperty(exports, "__esModule", { value: true });
+const _4_dummy_api_1 = require("./4-dummy-api");
+const resPromise = (0, _4_dummy_api_1.getData)('https://dummyjson.com/users');
+resPromise.then(value => console.log(value));
+// console.log(resPromise.then((value) => {
+//     console.log(value);
+// }));
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-const a = 1;
+import { getData } from "./4-dummy-api";
+
+const resPromise = getData('https://dummyjson.com/users');
+resPromise.then(value => console.log(value));
+
+// console.log(resPromise.then((value) => {
+//     console.log(value);
+// }));
