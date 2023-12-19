2a3e701a0d950faea875a97d742b7d676337c135
Dmitry Turchenko <dmitrytyt@ya.ru>
2023-12-19 22:22:51 +0300

    Add ignore js

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _4_dummy_api_1 = require("./4-dummy-api");
-const resPromise = (0, _4_dummy_api_1.getData)('https://dummyjson.com/users');
-resPromise.then(value => console.log(value));
-// console.log(resPromise.then((value) => {
-//     console.log(value);
-// }));
